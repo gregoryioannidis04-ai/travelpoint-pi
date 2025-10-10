51786a400c723dbe4d5a6dd4e8bb8b1f6a34b6dd
gregoryioannidis04-ai <gregoryioannidis04@gmail.com>
2025-10-11 01:57:41 +0300

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -22,7 +22,7 @@ module.exports = async function handler(req, res) {
     const response = await fetch( 'https://api.minepi.com/v2/payments/${paymentId}/approve', {
       method: 'POST',
       headers: {
-         'Authorization': Key ${process.env.PI_API_KEY},
+         Authorization:'Key ${process.env.PI_API_KEY'},
         'Content-Type': 'application/json',
       },
     });
