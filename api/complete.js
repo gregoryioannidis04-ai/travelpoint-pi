48af0919f7adcc10e31752e9544729c6fe0e5241
gregoryioannidis04-ai <gregoryioannidis04@gmail.com>
2025-10-11 01:59:27 +0300

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -22,7 +22,7 @@ module.exports = async function handler(req, res) {
     const response = await fetch( 'https://api.minepi.com/v2/payments/${paymentId}/complete', {
       method: 'POST',
       headers: {
-         'Authorization': Key ${process.env.PI_API_KEY},
+         Authorization: 'Key ${process.env.PI_API_KEY'},
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ txid }),
